420f640e81d6185089c43ddc99fff7a31106934f
mirch81 <mrmirch81@gmail.com>
2025-05-01 13:08:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
-import streamlit as st
+import os
+from dotenv import load_dotenv
 
-API_KEY = st.secrets["api"]["key"]
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 BASE_URL = "https://v3.football.api-sports.io"
 HEADERS = {
     "x-apisports-key": API_KEY
