0ec1bdfe8270b0877bdf8579e29e77a7ebe5d10c
mirch81 <mrmirch81@gmail.com>
2025-05-01 16:58:39 +0300

    Update form.py

--- form.py
+++ form.py
@@ -51,8 +51,8 @@ def get_team_last_matches(fixtures, team_name, max_matches=5):
         else:
             result_icon = "🤝"
 
-        # Maç ismini 5px daha büyük ve kalın yapıyoruz
-        summary = f"<div style='font-weight:bold; font-size:25px'>{date} – {team_name} vs {opponent} {result_icon} | MS: {home_goals}-{away_goals}</div>"
+        # Font büyüklüğünü 2px küçültüyoruz
+        summary = f"<div style='font-weight:bold; font-size:23px'>{date} – {team_name} vs {opponent} {result_icon} | MS: {home_goals}-{away_goals}</div>"
         st.markdown(summary, unsafe_allow_html=True)
 
         team_goals_list = get_team_goals(events, home_name)
