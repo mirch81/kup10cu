4ded744b337444cb8d863cc77c68cb250d33fec8
mirch81 <mrmirch81@gmail.com>
2025-05-01 17:09:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -75,12 +75,12 @@ if monthly_fixtures:
         st.markdown(f"### {team_home}")
         summaries = get_team_last_matches(all_fixtures, team_home)
         for line in summaries:
-            st.markdown(line, unsafe_allow_html=True)
+            st.markdown(line)
 
     with col2:
         st.markdown(f"### {team_away}")
         summaries = get_team_last_matches(all_fixtures, team_away)
         for line in summaries:
-            st.markdown(line, unsafe_allow_html=True)
+            st.markdown(line)
 else:
     st.warning("Seçilen filtrelere göre maç bulunamadı.")
