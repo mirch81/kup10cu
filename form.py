d1f8360b1e3551bf2bf4d446591466345dd200f8
mirch81 <mrmirch81@gmail.com>
2025-05-01 15:26:17 +0300

    Update form.py

--- form.py
+++ form.py
@@ -59,10 +59,8 @@ def get_team_last_matches(fixtures, team_name, max_matches=5):
         team_goals_min = get_goal_minutes(events, team_name)
         opp_goals_min = get_goal_minutes(events, opponent)
         if team_goals_min:
-            summary += "\n"
             summary += f"\n {team_name}: " + ', '.join([str(g) + "'" for g in team_goals_min])
         if opp_goals_min:
-            summary += "\n"
             summary += f"\n {opponent}: " + ', '.join([str(g) + "'" for g in opp_goals_min])
 
         result.append(summary)
